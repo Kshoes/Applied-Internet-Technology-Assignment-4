2b11996419d1432e44ab48d67a1db4cb3a071b7f
Kshoes <kx360@nyu.edu>
2019-10-09 12:34:16 -0400

    complete middleware and logging for debugging

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // app.js
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine', 'hbs');
+
+const logger = function(req, res, next) {   // custom middleware for debugging
+    console.log("Method: " + req.method);
+    console.log("Path: " + req.path);
+    console.log(req.query);
+    next();
+};
+
+app.use(logger);
+
+app.get('/', (req, res) => {    // routing
+    res.render('home');
+})
+
+app.get('/add', (req, res) => {
+    res.render('add');
+})
+
+app.listen(3000);
+
+console.log('Server started; type CTRL+C to shut down');
